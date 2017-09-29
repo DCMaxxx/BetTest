9b07a6c63dd39ae1069ba48ea6a22cff9ceff6d0
Maxime de Chalendar <maxime.dechalendar@me.com>
2017-09-29 10:46:29 +0200

    Added the project's skeleton

---
+++ project/BetTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BetTest
+//
+//  Created by Maxime de Chalendar on 29/09/2017.
+//  Copyright © 2017 Maxime de Chalendar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
