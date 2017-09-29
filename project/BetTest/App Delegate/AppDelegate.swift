3e9aa93e340d9d845a621e7daa9ab80ee45e62a8
Maxime de Chalendar <maxime.dechalendar@me.com>
2017-09-29 11:01:42 +0200

    - Organized the project on disk - Removed unnecessary code from Xcode's template

---
+++ project/BetTest/App Delegate/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  BetTest
+//
+//  Created by Maxime de Chalendar on 29/09/2017.
+//  Copyright © 2017 Maxime de Chalendar. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
---
+++ project/BetTestTests/Tests/BetTestTests.swift
@@ -0,0 +1,14 @@
+//
+//  BetTestTests.swift
+//  BetTestTests
+//
+//  Created by Maxime de Chalendar on 29/09/2017.
+//  Copyright © 2017 Maxime de Chalendar. All rights reserved.
+//
+
+import XCTest
+@testable import BetTest
+
+class BetTestTests: XCTestCase {
+    
+}
